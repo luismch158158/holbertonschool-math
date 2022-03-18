0653348e2369cc44dd4b90073dd32d28161ae8a0
Luis Manrique <luismanrique158158@gmail.com>
2022-03-18 16:37:59 +0000

    Fix betty style in holberton.h

---
+++ 0x00-math_complex/holberton.h
@@ -0,0 +1,30 @@
+#ifndef HOLBERTON_H
+#define HOLBERTON_H
+
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+ * struct complex_i - Structure to save Poppy information
+ * @re: real part
+ * @im: imaginary part
+ */
+
+typedef struct complex_i
+{
+	double re;
+	double im;
+} complex;
+
+void display_complex_number(complex c);
+complex conjugate(complex c);
+double modulus(complex c);
+double argument(complex c);
+void addition(complex c1, complex c2, complex *c3);
+void substraction(complex c1, complex c2, complex *c3);
+void multiplication(complex c1, complex c2, complex *c3);
+void division(complex c1, complex c2, complex *c3);
+void complex_from_mod_arg(double m, double arg, complex *c3);
+
+
+#endif /*HOLBERTON_H*/
