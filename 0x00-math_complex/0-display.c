062260141ed922ed58ebcb89cfa6010967c99fca
Luis Manrique <luismanrique158158@gmail.com>
2022-03-18 16:43:49 +0000

    Fix order in loops

--- 0x00-math_complex/0-display.c
+++ 0x00-math_complex/0-display.c
@@ -21,13 +21,13 @@ void display_complex_number(complex c)
 	{
 		printf("%.9g", c.re);
 	}
-	else if (c.im < 0)
-	{
-		printf("%.9g - %.9g", c.re, -c.im);
-	}
 	else if (c.im == -1)
 	{
 		printf("%.9g - i", c.re);
+	}
+	else if (c.im < 0)
+	{
+		printf("%.9g - %.9gi", c.re, -c.im);
 	}
 	else
 		printf("%.9g + %.9gi", c.re, c.im);
